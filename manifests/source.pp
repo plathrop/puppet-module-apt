83ed67bdb4f70bf82bf1125bfdcb429b9ccd2f00
Ian Eure <ian.eure@gmail.com>
2010-08-14 02:56:33 +0800

    Add repository key support.

--- manifests/source.pp
+++ manifests/source.pp
@@ -1,5 +1,5 @@
 define apt::source($enable=true, $deb_src=true, $url="", $dist="",
-                   $sections="main contrib non-free") {
+                   $sections="main contrib non-free", $key="") {
   include apt
 
   $distribution = $dist ? {
@@ -7,13 +7,30 @@ define apt::source($enable=true, $deb_src=true, $url="", $dist="",
     default => $dist,
   }
 
+  $keyexec = $key ? {
+    '': "noop",
+    default: "import-key"
+  }
+
   $list=inline_template("deb $url $distribution $sections
 <% if deb_src -%>
 deb-src $url $distribution $sections
 <% end %>")
 
+  exec {
+    "import-key":
+      path => "/usr/bin",
+      user => root,
+      command => "echo ${key} | apt-key add -";
+
+    "noop":
+      path => "/bin",
+      command => "true";
+  }
+
   file {
     "/etc/apt/sources.list.d/${name}.list":
+      require => Exec[$keyexec],
       mode => 0644,
       content => $enable ? {
         true => "${list}",
